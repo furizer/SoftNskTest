5c09c150fd20fc2ffc53eef0339e2f5840d27d8e
Evgeny Shmakov <82882097+furizer@users.noreply.github.com>
2022-09-01 19:44:09 +0300

    end project

--- dist/project.js
+++ dist/project.js
@@ -1,1 +1,25 @@
-console.log('ss'); 
+// simple range slider
+
+const rangeSlider = document.getElementById("rs-range-line");
+const rangeBullet = document.getElementById("rs-present");
+
+if(rangeSlider && rangeBullet) {
+    rangeSlider.addEventListener("input", () => {
+        rangeBullet.innerHTML = rangeSlider.value + '%';
+    });
+}
+
+
+// simple burger menu
+const burgerToggler = document.querySelector('.header__burger-open')
+const burgerList = document.querySelector('.header__burger-list')
+
+if(burgerToggler && burgerList) {
+    burgerToggler.addEventListener("click", (e) => {
+        // console.log(e.currentTarget);
+        e.currentTarget.classList.toggle('active')
+        burgerList.classList.toggle('active')
+    });
+}
+
+// create by Furizer
